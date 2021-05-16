d3e92180543c25392345a6ca1ddb8aaf26bd9fde
malshurman1 <56489756+malshurman1@users.noreply.github.com>
2021-05-16 16:36:30 +0200

    Add files via upload
    Added ASP.NET Core App template to repository

---
+++ Turisticka-agencija/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Turisticka_agencija.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
